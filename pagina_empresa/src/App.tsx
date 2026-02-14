377e61439b9e6e7223ab20c2ff1a23542ac0cd30
Javier Danilo Parra Quiñones <72566043+javierQ854@users.noreply.github.com>
2026-02-14 15:07:43 +0000

    Inicio de proyecto

---
+++ pagina_empresa/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-sky-50 ">
+      <div className="bg-white p-8 rounded-lg shadow-xl">
+        <h1 className="text-4xl font-bold text-red-500">
+          ¿Ves este texto ROJO?
+        </h1>
+        <p className="text-green-600 mt-4 text-xl">
+          ¿Y este texto VERDE?
+        </p>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ pagina_empresa/src/Components/Header/Header.tsx
@@ -0,0 +1,7 @@
+const Header = ()=>{
+    return(
+        <h1 className="text-blue-200">Este es el header</h1>
+    )
+}
+
+export default Header
